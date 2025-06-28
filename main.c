e790b6bea79cf1430e74434626f6db50d1fbd241
Hobby Programmer <info.hobbyprogrammer@gmail.com>
2025-06-28 19:11:30 +0530

    Update main.c
    Added comments

--- main.c
+++ main.c
@@ -1,3 +1,17 @@
+/*
+ * A random number guessing game in C.
+ *
+ * This program handles only numeric input.
+ * Invalid input (e.g., "five" or any non-integer) is **NOT** handled,
+ * to add that as a practice exercise.
+ *
+ * The program generates a random number between 1 and 100.
+ * After each guess, the program provides feedback:
+ * - "Above <input>" if the guess is too low
+ * - "Below <input>" if the guess is too high
+ * - "Success" message on correct guess
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,11 +21,11 @@ int main() {
     int target_number = 1 + rand() % 100;
     int input_number;
 
-    printf("I'm thiking of a number between 1-100, can you guess:\n");
+    printf("I'm thinking of a number between 1-100, can you guess:\n");  // Fixed typo: was "thiking" in video version
     while(1) {
         scanf("%d", &input_number);
         if(input_number == target_number) {
-            printf("success, it's correct\n");
+            printf("Success, it's correct.\n");
             break;
         }
         else if(input_number < target_number) {
@@ -21,5 +35,6 @@ int main() {
             printf("Below %d\n", input_number);
         }
     }
+
     return 0;
 }
